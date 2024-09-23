3f55a4fb3867ead5c320d33a324204a9ecb1905d
FelloMan <filip0816@hotmail.com>
2024-09-23 13:23:49 +0200

    Add project files.

---
+++ SUT24-Övning Kalkylator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SUT24_Övning_Kalkylator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

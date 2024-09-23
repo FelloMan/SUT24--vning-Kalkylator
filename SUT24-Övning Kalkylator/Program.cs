b91bbe93889d7bc76cb91d686dce19c50e071fe1
BingoDingo04 <viggo.kristensen@hotmail.com>
2024-09-23 14:21:47 +0200

    The menu works!!!

--- SUT24-Övning Kalkylator/Program.cs
+++ SUT24-Övning Kalkylator/Program.cs
@@ -1,69 +1,113 @@
-﻿namespace SUT24_Övning_Kalkylator
+﻿using System.Reflection.PortableExecutable;
+
+namespace SUT24_Övning_Kalkylator
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            //Menu
-            Console.WriteLine("Välj ett av alternativen");
+            int num1 = 0;
+            int num2 = 0;
+            int result = 0;
 
-            Console.WriteLine("1. Addition");
-            Console.WriteLine("2. Subtraktion");
-            Console.WriteLine("3. Multiplíkation");
-            Console.WriteLine("4. Division");
+            bool programOn = true;
 
-            string menuChoice = Console.ReadLine();
 
-            switch (menuChoice)
+            while (programOn)
             {
-                case "1":
-                    break;
-                case "2":
-                    break;
-                case "3":
-                    break;
-                case "4":
-                    break;
-                default:
-                    Console.WriteLine("Fel inmatning vid menyval");
-                    break;
+                //Menu
+                Console.WriteLine("Välj ett av alternativen");
+
+                Console.WriteLine("1. Addition");
+                Console.WriteLine("2. Subtraktion");
+                Console.WriteLine("3. Multiplíkation");
+                Console.WriteLine("4. Division");
+                Console.WriteLine("5. Avsluta program");
+
+                string menuChoice = Console.ReadLine();
+
+                switch (menuChoice)
+                {
+                    case "1":
+                        Console.WriteLine("Välj två nummer");
+                        num1 = Convert.ToInt32(Console.ReadLine());
+                        num2 = Convert.ToInt32(Console.ReadLine());
+
+                        result = Add(num1, num2);
+
+                        Console.WriteLine($"Resultatet blev: {result}");
+                        break;
+                    case "2":
+                        Console.WriteLine("Välj två nummer");
+                        num1 = Convert.ToInt32(Console.ReadLine());
+                        num2 = Convert.ToInt32(Console.ReadLine());
+
+                        result = Subtract(num1, num2);
+
+                        Console.WriteLine($"Resultatet blev: {result}");
+                        break;
+                    case "3":
+                        Console.WriteLine("Välj två nummer");
+                        num1 = Convert.ToInt32(Console.ReadLine());
+                        num2 = Convert.ToInt32(Console.ReadLine());
+
+                        result = Multiply(num1, num2);
+
+                        Console.WriteLine($"Resultatet blev: {result}");
+                        break;
+                    case "4":
+                        Console.WriteLine("Välj två nummer");
+                        num1 = Convert.ToInt32(Console.ReadLine());
+                        num2 = Convert.ToInt32(Console.ReadLine());
+
+                        result = Divison(num1, num2);
+
+                        Console.WriteLine($"Resultatet blev: {result}");
+                        break;
+                    case "5":
+                        programOn = false;
+                        break;
+                    default:
+                        Console.WriteLine("Fel inmatning vid menyval, försök igen");
+                        break;
+                }
             }
         }
 
 
 
 
-        public static int  Divison(int userDivison, int userDivison2)
+        public static int Divison(int userDivison, int userDivison2)
         {
             int userAnswear = userDivison / userDivison2;
             return userAnswear;
-        } 
-        
-
-        
+        }
 
 
-        public static int Add(int addFirstNumb, int addSecondNumb) 
-        { 
+
+
+
+        public static int Add(int addFirstNumb, int addSecondNumb)
+        {
             int sumAdd = addFirstNumb + addSecondNumb;
             return sumAdd;
-            
-        
+
+
         }
 
         public static int Subtract(int subFirstNumb, int subSecondNumb)
-        { 
+        {
             int sumSubtract = subFirstNumb - subSecondNumb;
             return sumSubtract;
-        
+
         }
 
-        public static int Multiply(int multiFirstNumb, int multiSecondNumb)  
-        { 
-            int multiplySum = multiFirstNumb * multiSecondNumb; 
+        public static int Multiply(int multiFirstNumb, int multiSecondNumb)
+        {
+            int multiplySum = multiFirstNumb * multiSecondNumb;
             return multiplySum;
-            
-        
+
+
         }
 
     }
